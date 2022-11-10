c0b9cf260a735747fa5b5c404a1005f5c1be56e6
LucaSamore <luca.samore.01@gmail.com>
2022-11-10 16:28:48 +0100

    Test tiles coloring

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +7,36 @@ public class PlayerController : MonoBehaviour
 {
     private static readonly float SPEED = 5f;
 
-    [SerializeField] private CharacterController _characterController;
+    [SerializeField] 
+    private CharacterController _characterController;
+
+    private float _gravity;
 
     // Start is called before the first frame update
     void Start()
     {
+        _gravity = -9.81f * Time.deltaTime;
         _characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+    
+    private void MovePlayer()
+    {
         var x = Input.GetAxis("Horizontal") * SPEED * Time.deltaTime;
         var z = Input.GetAxis("Vertical") * SPEED * Time.deltaTime;
-        _characterController.Move(new Vector3(x, 0, z));
+        _characterController.Move(new Vector3(x, _gravity, z));
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.name == "Cube")
+        {
+            hit.gameObject.GetComponent<Renderer>().material.color = Color.green;
+        }
     }
 }
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Scripting;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
