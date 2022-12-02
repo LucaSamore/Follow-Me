8de5833b86f2dcc2339187d1a54b8f9a8617e6bc
LucaSamore <luca.samore.01@gmail.com>
2022-12-02 10:49:20 +0100

    Add custom navmesh factory implementation

--- Assets/Scripts/Map/CustomNavMesh/INavMeshFactory.cs
+++ Assets/Scripts/Map/CustomNavMesh/INavMeshFactory.cs
@@ -1,7 +1,10 @@
-﻿namespace Map.CustomNavMesh
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Map.CustomNavMesh
 {
-    public interface INavMeshFactory
+    public interface INavMeshFactory<T>
     {
-        
+        IDictionary<Vector3,T> Bake(Transform zone, Vector3 startingPosition);
     }
 }
--- Assets/Scripts/Map/CustomNavMesh/NavMeshFactory2D.cs
+++ Assets/Scripts/Map/CustomNavMesh/NavMeshFactory2D.cs
@@ -1,7 +1,11 @@
-﻿namespace Map.CustomNavMesh
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Map.CustomNavMesh
 {
-    public sealed class NavMeshFactory2D : INavMeshFactory
+    public sealed class NavMeshFactory2D : INavMeshFactory<Vector2Int>
     {
-        
+        public IDictionary<Vector3, Vector2Int> Bake(Transform zone, Vector3 startingPosition) =>
+            MapUtil.Map2D(zone, startingPosition);
     }
 }
