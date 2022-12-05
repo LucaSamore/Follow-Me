994e2015fd735a50f071b187a2bb3ecccb2dc8c0
LucaSamore <luca.samore.01@gmail.com>
2022-12-05 10:15:45 +0100

    Implement template method over tweaked dijkstra's algorithm

---
+++ Assets/Scripts/Characters/AI/Algorithms/2D/TweakedDijkstra2Dv2.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using Characters.AI.Algorithms.Util;
+using UnityEngine;
+using System.Linq;
+
+namespace Characters.AI.Algorithms._2D
+{
+    public sealed class TweakedDijkstra2Dv2 : TweakedDijkstra<Vector2Int>
+    {
+        protected override IList<Node<Vector2Int>> FindShortestPath(Node<Vector2Int> destination)
+        {
+            var end = false;
+            var path = new List<Node<Vector2Int>>();
+            var closedNodes = new List<Node<Vector2Int>>();
+            var openNodes = OpenNodesWithMinimumCost();
+
+            while (!end)
+            {
+                foreach (var node in openNodes)
+                {
+                    node.State = NodeState.Close;
+                    var nonClosedNeighbours = NonClosedNeighbours(node);
+                    nonClosedNeighbours.ToList().ForEach(n => n.State = NodeState.Open);
+
+                    nonClosedNeighbours.ToList().ForEach(n =>
+                    {
+                        var neighbourWithMinCost = NeighbourWithMinimumCost(n);
+
+                        if (TryUpdateCost(n, neighbourWithMinCost))
+                        {
+                            n.Parent = neighbourWithMinCost;
+                        }
+                    });
+                
+                    closedNodes.Add(node);
+                }
+                
+                end = closedNodes
+                    .Select(cn => cn.Element)
+                    .Where(v2 => v2.Equals(destination.Element))
+                    .ToArray().Length == 1;
+                
+                openNodes = OpenNodesWithMinimumCost();
+            }
+
+            var current = destination;
+            path.Add(current);
+
+            while (current.Parent is not null)
+            {
+                path.Add(current.Parent);
+                current = current.Parent;
+            }
+
+            path.Reverse();
+
+            return path;
+        }
+        
+        protected override int SetIterations(Node<Vector2Int> destination) =>
+            Enumerable
+                .Range(destination.Element.x - 1, NeighbourDistance)
+                .SelectMany(kk => Enumerable.Range(destination.Element.y - 1, NeighbourDistance)
+                    .Select(vv => new Vector2Int(kk, vv)))
+                .Where(p => Nodes.Select(n => n.Element).Contains(p))
+                .SelectMany(p => Nodes.ToList().Where(n => n.Element.Equals(p)))
+                .Count(n => n.State != NodeState.Used) - 1;
+        
+        protected override IList<Node<Vector2Int>> NonClosedNeighbours(Node<Vector2Int> node) =>
+            Enumerable.Range(node.Element.x - 1, NeighbourDistance)
+                .SelectMany(kk =>
+                    Enumerable.Range(node.Element.y - 1, NeighbourDistance).Select(vv => new Vector2Int(kk, vv)))
+                .Where(p => Nodes.Select(n => n.Element).Contains(p))
+                .SelectMany(p => Nodes.ToList().Where(n => n.Element.Equals(p)))
+                .Where(n => n.State != NodeState.Close && n.State != NodeState.Used)
+                .ToList();
+
+        protected override Node<Vector2Int> NeighbourWithMinimumCost(Node<Vector2Int> node)
+        {
+            var neighbours = Enumerable.Range(node.Element.x - 1, NeighbourDistance)
+                .SelectMany(kk =>
+                    Enumerable.Range(node.Element.y - 1, NeighbourDistance).Select(vv => new Vector2Int(kk, vv)))
+                .Where(p => Nodes.Select(n => n.Element).Contains(p))
+                .SelectMany(p => Nodes.ToList().Where(n => n.Element.Equals(p)))
+                .Where(n => n.State != NodeState.Used);
+            
+            return neighbours.First(n => n.Cost == GetMinimumCost(neighbours));
+        }
+
+        protected override bool TryUpdateCost(Node<Vector2Int> toBeUpdated, Node<Vector2Int> from)
+        {
+            var cost = from.Cost + (NodeUtil.IsDiagonalNeighbour(from, toBeUpdated)
+                ? DiagonalCost
+                : HorizontalAndVerticalCost);
+
+            if (cost > toBeUpdated.Cost) return false;
+            toBeUpdated.Cost = cost;
+            return true;
+        }
+
+        protected override void ResetMap()
+        {
+            Nodes
+                .Where(n => !n.Element.Equals(Vector2Int.zero))
+                .Where(n => n.State != NodeState.Used)
+                .ToList()
+                .ForEach(n =>
+                {
+                    n.Cost = int.MaxValue;
+                    n.Parent = null;
+                    n.State = NodeState.None;
+                });
+
+            Nodes.First(n => n.Element.Equals(Vector2Int.zero)).State = NodeState.Open;
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/AI/Algorithms/TweakedDijkstra.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Characters.AI.Algorithms.Util;
+using UnityEngine;
+using System.Linq;
+using Map;
+
+namespace Characters.AI.Algorithms
+{
+    public abstract class TweakedDijkstra<T> : IPathStrategy<T> where T : struct
+    {
+        protected static readonly int NeighbourDistance = 3;
+        protected static readonly int HorizontalAndVerticalCost = 10;
+        protected static readonly int DiagonalCost = 14;
+        
+        protected int _algorithmIterations;
+        protected readonly ISet<IList<Node<T>>> _generatedPaths = new HashSet<IList<Node<T>>>();
+        protected List<Node<T>> Nodes { get; set; }
+        
+        public IList<Tuple<Vector3,T>> CreatePath(IDictionary<Vector3,T> map, T startingPosition, int depth)
+        {
+            Nodes = NodeUtil.MapToNodes(map).ToList();
+            var source = new Node<T>(startingPosition) { Cost = 0, State = NodeState.Open };
+            var destination = ChooseDestination(depth);
+            _algorithmIterations = SetIterations(destination);
+            Debug.Log($"Destination: {destination.Element}");
+            Debug.Log($"Iterations: {_algorithmIterations}");
+            
+            for (var i = 0; i < _algorithmIterations; i++)
+            {
+                Nodes.Add(source);
+                var path = FindShortestPath(destination);
+                _generatedPaths.Add(path);
+                ResetMap();
+                RemovePathFromMap(path);
+            }
+
+            var finalPath = ChooseRandomPath();
+
+            return finalPath.Select(n =>
+                    new Tuple<Vector3,T>(MapUtil.GetKeyFromValue(map, n.Element), n.Element))
+                .ToList();
+        }
+
+        protected abstract int SetIterations(Node<T> destination);
+        
+        protected abstract IList<Node<T>> FindShortestPath(Node<T> destination);
+        
+        protected abstract Node<T> NeighbourWithMinimumCost(Node<T> node);
+
+        protected abstract IList<Node<T>> NonClosedNeighbours(Node<T> node);
+
+        protected abstract bool TryUpdateCost(Node<T> toBeUpdated, Node<T> from);
+
+        protected abstract void ResetMap();
+        
+        protected virtual Node<T> ChooseDestination(int depth)
+        {
+            // var rng = new Random();
+            // var items = Nodes
+            //     .Where(n => Math.Abs(n.Element.x) >= depth || 
+            //              Math.Abs(n.Element.y) >= depth)
+            //     .ToList();
+            //
+            // return items.OrderBy(i => rng.Next()).First();
+            
+            return Nodes[4];
+        }
+        
+        protected IList<Node<T>> OpenNodesWithMinimumCost() =>
+            Nodes
+                .Where(n => n.State == NodeState.Open)
+                .Where(on => on.Cost == NodeWithMinimumCost().Cost)
+                .ToList();
+        
+        protected Node<T> NodeWithMinimumCost() =>
+            Nodes.First(n => n.Cost == Nodes
+                .Where(n1 => n1.State == NodeState.Open)
+                .Select(n1 => n1.Cost).Min());
+        
+        protected int GetMinimumCost(IEnumerable<Node<T>> nodes) =>
+            nodes.Select(n => n.Cost).Min();
+
+        protected void RemovePathFromMap(ICollection<Node<T>> path)
+        {
+            Nodes
+                .Where(n => path
+                    .ToList()
+                    .GetRange(1, path.Count - 2)
+                    .Select(x => x.Element)
+                    .Contains(n.Element))
+                .ToList()
+                .ForEach(n => n.State = NodeState.Used);
+        }
+        
+        protected IList<Node<T>> ChooseRandomPath() => _generatedPaths.Count > 0 ? 
+            _generatedPaths.ElementAt(new System.Random().Next(0, _generatedPaths.Count - 1)) : new List<Node<T>>();
+    }
+}
