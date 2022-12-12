72a7cb3296ad2b7358f9c7e929da2181364b6cf2
LucaSamore <luca.samore.01@gmail.com>
2022-12-12 09:48:27 +0100

    Add map documentation

--- Assets/Scripts/Map/MapUtil.cs
+++ Assets/Scripts/Map/MapUtil.cs
@@ -5,8 +5,21 @@ using System.Linq;
 
 namespace Map
 {
+    /// <summary>
+    /// Provides a set of utility methods used for managing the character's map.
+    /// </summary>
     public static class MapUtil
     {
+        /// <summary>
+        /// Converts the Unity coordinate system to a custom coordinate system, which is the distance from a starting
+        /// point. This method is used by <c>INavMeshFactory</c> for creating a custom NavMesh.
+        /// </summary>
+        /// <param name="zone">Where the character can move, using Unity coordinate system.</param>
+        /// <param name="startingPosition">The origin of the custom coordinate system.</param>
+        /// <returns>
+        /// A <c>IDictionary</c> describing the NavMesh. Keys are the original positions.
+        /// Values are all the positions mapped to the custom coordinate system.
+        /// </returns>
         public static IDictionary<Vector3,Vector2Int> Map2D(Transform zone, Vector3 startingPosition) =>
             zone
                 .Cast<Transform>()
@@ -15,10 +28,28 @@ namespace Map
                     v => Vector2Int.RoundToInt(
                         new Vector2(v.x - startingPosition.x, v.z - startingPosition.z)));
 
+        /// <summary>
+        /// Converts the Unity coordinate system to a custom coordinate system, which is the distance from a starting
+        /// point. This method is used by <c>INavMeshFactory</c> for creating a custom NavMesh.
+        /// </summary>
+        /// <param name="zone">Where the character can move, using Unity coordinate system.</param>
+        /// <param name="startingPosition">The origin of the custom coordinate system.</param>
+        /// <returns>
+        /// A <c>IDictionary</c> describing the NavMesh. Keys are the original positions.
+        /// Values are all the positions mapped to the custom coordinate system.
+        /// </returns>
         public static IDictionary<Vector3, Vector3Int> Map3D(Transform zone, Vector3 startingPosition) =>
             throw new NotImplementedException();
 
-        public static Vector3 GetKeyFromValue<T>(IDictionary<Vector3,T> map, T v2) =>
-            map.FirstOrDefault(kvp => kvp.Value.Equals(v2)).Key;
+        /// <summary>
+        /// Given an <c>IDictionary</c> and a value, it returns the first key associated with that value.
+        /// </summary>
+        /// <param name="map">An <c>IDictionary</c> instance.</param>
+        /// <param name="value">The value.</param>
+        /// <typeparam name="K">The type of the key.</typeparam>
+        /// <typeparam name="V">The type of the value.</typeparam>
+        /// <returns>The first key associated with that value.</returns>
+        public static K GetKeyFromValue<K,V>(IDictionary<K,V> map, V value) =>
+            map.FirstOrDefault(kvp => kvp.Value.Equals(value)).Key;
     }
 }
--- Assets/Scripts/Map/TileController.cs
+++ Assets/Scripts/Map/TileController.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 namespace Map
 {
+    /// <summary>
+    /// Represents a controller attached to the characters that manage the collision
+    /// with the collider of a single tile.
+    /// </summary>
     public sealed class TileController : MonoBehaviour
     {
         private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
@@ -10,14 +14,16 @@ namespace Map
         public Color color = Color.green;
         
         [CanBeNull] private Renderer _previousTile;
+        
         private void OnTriggerEnter(Collider other)
         {
+            // Change tile's color when the character steps on it.
             if (other.name != "Cube") return;
-            //if (_previousTile is not null) _previousTile.material.SetColor(EmissionColor, Color.black);
+            if (_previousTile is not null) _previousTile.material.SetColor(EmissionColor, Color.black);
             var renderer = other.gameObject.GetComponent<Renderer>();
             renderer.material.EnableKeyword("_EMISSION");
             renderer.material.SetColor(EmissionColor, color);
-            //_previousTile = renderer;
+            _previousTile = renderer;
         }
     }
 }
